dc9359eaccc646d1aab1066fab4a80a7db441571
komalsani3030 <komaldhiangla@gmail.com>
2020-08-17 16:55:26 +0530

    complete project

--- Komal/src/app/home/home.component.ts
+++ Komal/src/app/home/home.component.ts
@@ -1,24 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
-  template: `
-  <div class="row">
-  <div class="main">
- 
-  <div class="card" style="height: 550px;">
-        
-      </div>
-  </div>
-</div>
-
-  `,
-  styleUrls: ['../main.css']
+  templateUrl: `./home.component.html`,
+  styleUrls: ['./home.css']
 })
 export class HomeComponent implements OnInit {
 
+  startIndex = 0;
   constructor() { }
-
+  ImageData = ["/assets/banking12.jpg","/assets/sider1.jpg","/assets/image2.jpg"];
   ngOnInit(): void {
+    this.Repeat();
+  }
+  Repeat() {
+    setTimeout(() => {
+      this.__FunctionSlide();
+      this.Repeat();
+    }, 2000);
   }
 
+  __FunctionSlide() {
+    const slides = Array.from(document.getElementsByClassName('mall-show-slide'));
+    if (slides === []) {
+      this.Repeat();
+    }
+    for (const x of slides) {
+      const y = x as HTMLElement;
+      y.style.display = 'none';
+    }
+    if (this.startIndex > slides.length - 1) {
+      this.startIndex = 0;
+      const slide = slides[this.startIndex] as HTMLElement;
+      slide.style.display = 'block';
+      this.startIndex++;
+    } else {
+
+      const slide = slides[this.startIndex] as HTMLElement;
+      slide.style.display = 'block';
+      this.startIndex++;
+    }
+  }
 }
